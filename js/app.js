398960e93a572a24b3fc2c325b33cce5c4f97e28
Niilaryea <niilaaye@gmail.com>
2019-01-21 20:35:09 -0500

    refactor: Update matching logic and animations - dry up animations - dry up card selections

--- js/app.js
+++ js/app.js
@@ -7,21 +7,11 @@
  * @property {element} target
  */
 
-// Shuffle function from http://stackoverflow.com/a/2450976
-function shuffle(array) {
-  var currentIndex = array.length, temporaryValue, randomIndex;
-
-  while (currentIndex !== 0) {
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
-    temporaryValue = array[currentIndex];
-    array[currentIndex] = array[randomIndex];
-    array[randomIndex] = temporaryValue;
-  }
-
-  return array;
-}
-
+let clickCount = 0;
+let starLevel = 0;
+const deck = document.body.querySelector('.deck');
+const MAX_NUMBER_OF_CLICKS = 2;
+const selectedCards = [];
 const cards = [
   'diamond',
   'diamond',
@@ -40,79 +30,129 @@ const cards = [
   'bomb',
   'bomb'
 ];
-const selectedCards = [];
-const gameBoard = document.body.querySelector('.deck');
+deck.addEventListener('click', clickHandler);
 
-gameBoard.addEventListener('click', clickHandler);
+// Shuffle function from http://stackoverflow.com/a/2450976
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  while (currentIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
 
 /**
-* @description Adds one or more list item elements to the DOM
-* @param {element} element - The DOM element to which the list items are added
-* @param {string[]} array - A list of one or more list items
-*/
-function addItems(element, array) {
+ * @description Adds cards to the DOM
+ * @param {element} deck - The DOM element to which the cards are added
+ * @param {string[]} cards - A list of cards
+ */
+function setGameBoard(deck, cards) {
   let fragment = document.createDocumentFragment();
 
-  array.forEach(item => {
+  cards.forEach(card => {
     let parentElement = document.createElement('li');
     let childElement = document.createElement('i');
+
     parentElement.setAttribute('class', 'card');
-    childElement.setAttribute('class', `fa fa-${item}`);
+    childElement.setAttribute('class', `fa fa-${card}`);
     parentElement.appendChild(childElement);
     fragment.appendChild(parentElement);
   });
 
-  element.appendChild(fragment);
+  deck.appendChild(fragment);
 }
 
 /**
-* @description Click event handler
-* @param {document#event:click} event
-* @listens document#click
-*/
-function clickHandler(event) {
-  if (event.target.classList.contains('card') && !event.target.classList.contains('match') && !event.target.classList.contains('open')) {
-    if (!selectedCards.length) {
-      selectedCards.push(event.target);
-      toggleClass(selectedCards[0], 'open', 'show');
+ * @description Tests if a click event is valid
+ * @param {document#event:click} event
+ * @param {number} maxNumberOfClicks
+ * @param {number} numberOfClicks
+ */
+function isValidClick(event, maxNumberOfClicks, numberOfClicks) {
+  return (
+    event.target.classList.contains('card') &&
+    !event.target.classList.contains('match') &&
+    !event.target.classList.contains('open') &&
+    numberOfClicks < maxNumberOfClicks
+  );
+}
+
+/**
+ * @description Tests if 2 cards are a match
+ * @param {element[]} cards - An array of 2 card elements
+ */
+function isAMatch(cards) {
+  return (
+    cards[0].firstChild.className === cards[1].firstChild.className
+  );
+}
+
+/**
+ * @description Flips 1 card by toggling one or more classes on the card element
+ * @param {element} card - The DOM element to which the classes are added
+ * @param {string[]} classList- A list of one or more classes
+ */
+function flipCard(card, ...classList) {
+  classList.forEach(className => {
+    card.classList.toggle(`${className}`);
+  });
+}
+
+/**
+ * @description Flips 2 card by toggling one or more classes on the card element
+ * @param {element[]} cards - The DOM elements to which the classes are added
+ * @param {string[]} classList- A list of one or more classes
+ */
+function flipCards(cards, ...classList) {
+  cards.forEach(card => {
+    flipCard(card, ...classList);
+  });
+}
+
+/**
+ * @description Tests if the selected cards match
+ * @param {string[]} cards
+ */
+function animateCards(cards, matchStatus, ...animationList) {
+  let classList = [matchStatus, ...animationList];
+  flipCards(cards, ...classList);
+  setTimeout(() => {
+    flipCards(cards, ...animationList);
+  }, 650);
+
+  setTimeout(() => {
+    if (matchStatus == 'no-match') {
+      flipCards(cards, 'no-match', 'open', 'show');
     }
+    cards.length = 0;
+  }, 750);
+}
 
-    else {
-      selectedCards.push(event.target);
+/**
+ * @description Click event handler
+ * @param {document#event:click} event
+ * @listens document#click
+ */
+function clickHandler(event) {
+  if (isValidClick(event, MAX_NUMBER_OF_CLICKS, selectedCards.length)) {
+    selectedCards.push(event.target);
+    flipCard(event.target, 'open', 'show');
 
-      if (selectedCards[0].firstChild.className === selectedCards[1].firstChild.className) {
-        toggleClass(selectedCards[1], 'open', 'show');
-        toggleClass(selectedCards[0], 'match', 'rubberBand', 'animated');
-        toggleClass(selectedCards[1], 'match', 'rubberBand', 'animated');
-        window.setTimeout(toggleClass, 450, selectedCards[0], 'rubberBand', 'animated');
-        window.setTimeout(toggleClass, 450, selectedCards[1], 'rubberBand', 'animated');
+    if (selectedCards.length == MAX_NUMBER_OF_CLICKS) {
+      if (isAMatch(selectedCards)) {
+        animateCards(selectedCards, 'match', 'rubberBand', 'animated');
+      } else {
+        animateCards(selectedCards, 'no-match', 'wobble', 'animated');
       }
-
-      else {
-        toggleClass(selectedCards[1], 'open', 'show');
-        toggleClass(selectedCards[0], 'no-match', 'wobble', 'animated');
-        toggleClass(selectedCards[1], 'no-match', 'wobble', 'animated');
-        window.setTimeout(toggleClass, 450, selectedCards[0], 'wobble', 'animated');
-        window.setTimeout(toggleClass, 450, selectedCards[1], 'wobble', 'animated');
-        window.setTimeout(toggleClass, 900, selectedCards[0], 'no-match', 'open', 'show');
-        window.setTimeout(toggleClass, 900, selectedCards[1], 'no-match', 'open', 'show');
-      }
-
-      selectedCards.length = 0;
     }
   }
 }
 
-/**
-* @description Toggles a list of classes on a DOM element
-* @param {element} element - The DOM element to which the classes are added
-* @param {string[]} array - A list of one or more classes
-*/
-function toggleClass(element, ...array) {
-  array.forEach(item => {
-    element.classList.toggle(`${item}`);
-  });
-}
-
 //shuffle(cards);
-addItems(gameBoard, cards);
+setGameBoard(deck, cards);
