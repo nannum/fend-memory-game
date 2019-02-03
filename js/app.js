95346ea4702ccb98a5dac8df5d73f1b680caac29
laryea annum <nannum@laryeas-MacBook-Air.local>
2019-02-03 16:37:15 -0500

    refactor: Apply DRY and KISS to all code

--- js/app.js
+++ js/app.js
@@ -25,19 +25,25 @@ const cards = [
   'bomb',
   'bomb'
 ];
-const deck = document.body.querySelector('.deck');
 const stars = document.body.querySelector('.stars');
 const moves = document.body.querySelector('.moves');
-const MAX_NUMBER_OF_CLICKS = 2;
-const MAX_NUMBER_OF_MATCHES = cards.length;
-const MAX_NUMBER_OF_STARS = stars.children.length;
-const selectedCards = [];
-let moveCount = 0;
-let starRating = MAX_NUMBER_OF_STARS;
 
-deck.addEventListener('click', cardClickHandler);
+const deck = document.body.querySelector('.deck');
+const MAX_NUMBER_OF_STARS = stars.children.length;
+const MAX_NUMBER_OF_MATCHES = cards.length;
+const MAX_NUMBER_OF_CLICKS = 2;
+const selectedCards = [];
+
+let moveCount = 0;
+let numberOfMoves = 0;
+let starRating = 0;
 
 // Shuffle function from http://stackoverflow.com/a/2450976
+/**
+ * @description Shuffles an array
+ * @param {array[]} array
+ * @returns {array[]}
+ */
 function shuffle(array) {
   var currentIndex = array.length, temporaryValue, randomIndex;
 
@@ -53,114 +59,134 @@ function shuffle(array) {
 }
 
 /**
- * @description Adds cards to the DOM
- * @param {element} deck - The DOM element to which the cards are added
+ * @description Sets the game board by adding card elements to a DOM fragment
  * @param {string[]} cards - A list of cards
+ * @returns {DocumentFragment}
  */
-function setGameBoard(deck, cards) {
+function setGameBoard(cards) {
   let fragment = document.createDocumentFragment();
 
   cards.forEach(card => {
-    let parentElement = document.createElement('li');
-    let childElement = document.createElement('i');
+    let cardElement = document.createElement('li');
+    let symbolElement = document.createElement('i');
 
-    parentElement.setAttribute('class', 'card');
-    childElement.setAttribute('class', `fa fa-${card}`);
-    parentElement.appendChild(childElement);
-    fragment.appendChild(parentElement);
+    cardElement.setAttribute('class', 'card');
+    symbolElement.setAttribute('class', `fa fa-${card}`);
+    cardElement.appendChild(symbolElement);
+    fragment.appendChild(cardElement);
   });
 
-  deck.appendChild(fragment);
+  return fragment;
 }
 
 /**
- * @description Tests if a click event is valid
- * @param {document#event:click} event
- * @param {number} maxNumberOfClicks
- * @param {number} numberOfClicks
+ * @description Tests if a click event target is valid
+ * @param {document#event:click} target - The click event target
  */
-function isValidClick(event, maxNumberOfClicks, numberOfClicks) {
+function isValidTarget(target) {
   return (
-    event.target.classList.contains('card') &&
-    !event.target.classList.contains('match') &&
-    !event.target.classList.contains('open') &&
-    numberOfClicks < maxNumberOfClicks
+    target.classList.contains('card') &&
+    !target.classList.contains('match') &&
+    !target.classList.contains('open')
   );
+}
+
+/**
+ * @description Tests if the number stored click events is valid
+ * @param {number} numberOfClicks - The current number of click events recorded
+ * @param {number} maxNumberOfClicks - The maximum number of click events allowed
+ */
+function isValidNumberOfClicks(numberOfClicks, maxNumberOfClicks) {
+  return numberOfClicks < maxNumberOfClicks;
 }
 
 /**
  * @description Tests if 2 cards are a match
  * @param {element[]} cards - An array of 2 card elements
  */
-function isAMatch(cards) {
-  return (
-    // cards[0].firstChild.className === cards[1].firstChild.className
-    cards[0].firstElementChild.className === cards[1].firstElementChild.className
-  );
+function isMatch(cards) {
+  return cards[0].firstElementChild.className === cards[1].firstElementChild.className;
 }
 
 /**
  * @description Tests to see if the game is over
- * @param {number} maxNumberOfClicks
  */
-function isGameOver(maxNumberOfMatches) {
+function isGameOver() {
   let matches = document.querySelectorAll('.match');
-  return (
-    matches.length === maxNumberOfMatches
-  );
+  let cards = document.querySelectorAll('.card');
+  return matches.length === cards.length;
 }
 
 /**
- * @description Flips 1 card by toggling one or more classes on the card element
- * @param {element} card - The DOM element to which the classes are added
+ * @description Toggles one or more classes on one element
+ * @param {element} element - The DOM element to which the classes are added
  * @param {string[]} classList- A list of one or more classes
  */
-function flipCard(card, ...classList) {
+function toggleClasses(element, ...classList) {
   classList.forEach(className => {
-    card.classList.toggle(`${className}`);
+    element.classList.toggle(`${className}`);
   });
 }
 
 /**
- * @description Flips 2 card by toggling one or more classes on the card element
- * @param {element[]} cards - The DOM elements to which the classes are added
+ * @description Toggles one or more classes on multiple elements
+ * @param {element[]} elements - The DOM elements to which the classes are added
  * @param {string[]} classList- A list of one or more classes
  */
-function flipCards(cards, ...classList) {
-  cards.forEach(card => {
-    flipCard(card, ...classList);
+function toggleElements(elements, ...classList) {
+  elements.forEach(element => {
+    toggleClasses(element, ...classList);
   });
 }
 
 /**
- * @description Tests if the selected cards match
- * @param {string[]} cards
+ * @description Animates a set of matching cards
+ * @param {element[]} cards - A list of cards
  */
-function animateCards(cards, matchStatus, ...animationList) {
-  let classList = [matchStatus, ...animationList];
-  flipCards(cards, ...classList);
+function animateMatch(cards) {
+  toggleElements(cards, 'match', 'rubberBand', 'animated');
+
   setTimeout(() => {
-    flipCards(cards, ...animationList);
+    toggleElements(cards, 'rubberBand', 'animated');
+  }, 550);
+}
+
+/**
+ * @description Animates a set of cards that don't match
+ * @param {element[]} cards - A list of cards
+ */
+function animateMisMatch(cards) {
+  toggleElements(cards, 'no-match', 'wobble', 'animated');
+
+  setTimeout(() => {
+    toggleElements(cards, 'no-match', 'wobble', 'animated');
   }, 550);
 
   setTimeout(() => {
-    if (matchStatus == 'no-match') {
-      flipCards(cards, 'no-match', 'open', 'show');
-    }
-    cards.length = 0;
-  }, 650);
+    toggleElements(cards, 'show', 'open');
+    // toggleElements(cards, 'open', 'show');
+  }, 600);
 }
 
 /**
- * @description Tests if the selected cards match
- * @param {string[]} cards
+ * @description Returns the current star rating
+ * @param {number} numberOfMoves - The current number of moves the user has made
  */
-function removeStar(star, rating) {
-  if (star.parentNode.children.length > rating) {
-    flipCard(star, 'rotateOut', 'animated');
-    setTimeout(() => {
-        star.parentNode.removeChild(star);
-    }, 600);
+function getStarRating(numberOfMoves) {
+  return (numberOfMoves <= 11) ? 3
+    : (numberOfMoves > 11 && numberOfMoves <= 16) ? 2
+    : (numberOfMoves > 16 && numberOfMoves <= 20) ? 1
+    : 0;
+}
+
+/**
+ * @description Removes stars from view
+ * @param {element[]} stars - The star DOM element
+ * @param {number} rating - The current star rating
+ */
+function removeStar(stars, rating) {
+  if (!stars.children[rating].classList.contains('rotateOut')) {
+    toggleClasses(stars.children[rating], 'rotateOut', 'animated');
   }
 }
 
@@ -170,39 +196,30 @@ function removeStar(star, rating) {
  * @listens document#click
  */
 function cardClickHandler(event) {
-  if (isValidClick(event, MAX_NUMBER_OF_CLICKS, selectedCards.length)) {
+  if (isValidTarget(event.target) && isValidNumberOfClicks(selectedCards.length, MAX_NUMBER_OF_CLICKS)) {
     selectedCards.push(event.target);
-    flipCard(event.target, 'open', 'show');
+    toggleClasses(event.target, 'open', 'show');
 
     if (selectedCards.length == MAX_NUMBER_OF_CLICKS) {
-      moveCount++;
-      moves.innerHTML = moveCount;
-      if (moveCount > 11 && moveCount <= 16) {
-        starRating = 2;
-        removeStar(stars.lastElementChild, starRating);
+      numberOfMoves++;
+      moves.innerHTML = numberOfMoves;
+      starRating = getStarRating(numberOfMoves);
+
+      if (starRating < MAX_NUMBER_OF_STARS) {
+        removeStar(stars, starRating);
       }
 
-      if (moveCount > 16 && moveCount <= 20) {
-        starRating = 1;
-        removeStar(stars.lastElementChild, starRating);
-      }
+      (isMatch(selectedCards)) ?
+        animateMatch(selectedCards) :
+        animateMisMatch(selectedCards);
 
-      if (moveCount > 20 && starRating > 0) {
-        starRating = 0;
-        removeStar(stars.lastElementChild, starRating);
-      }
-
-      if (isAMatch(selectedCards)) {
-        animateCards(selectedCards, 'match', 'rubberBand', 'animated');
-        if (isGameOver(MAX_NUMBER_OF_MATCHES)) {
-          gameOver();
-        }
-      } else {
-        animateCards(selectedCards, 'no-match', 'wobble', 'animated');
-      }
+      setTimeout(() => {
+        selectedCards.length = 0;
+      }, 650);
     }
   }
 }
 
 //shuffle(cards);
-setGameBoard(deck, cards);
+deck.appendChild(setGameBoard(cards));
+deck.addEventListener('click', cardClickHandler);
